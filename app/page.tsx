76fdea04b518b4387614b22d87638d8140d7bb3c
Duc-Hoang-24 <dnguy311@my.centennialcollege.ca>
2025-11-27 20:41:47 -0500

    Change text color to white for quotes
    Updated text color for blockquote and cite elements to white.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='absolute flex flex-col items-center justify-center top-1/3 translate-x-1/2 right-1/2 overflow-hidden'>
-  <blockquote className='text-4xl text-center max-w-full'>
+  <blockquote className='text-4xl text-center max-w-full text-white'>
     &quot; Flashcards are not about cramming facts, but about building lasting knowledge &quot;
   </blockquote>
-  <cite className='text-3xl text-right max-w-2xl w-2/2'>
+  <cite className='text-3xl text-right max-w-2xl w-2/2 text-white'>
     -Study Wisdom-
   </cite>
 </div>
